baa91d26d5e37dbf065d66f79d6f5079b457c9c5
Federico Rampazzo <frampone@gmail.com>
2015-09-02 22:53:23 +0100

    Fix moment deprecated notice and update everything

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function (dust, moment) {
   dust.helpers = dust.helpers || {}
   dust.helpers.formatDate = function(chunk, context, bodies, params) {
-    var date = dust.helpers.tap(params.date, chunk, context)
+    var date = dust.helpers.tap(params.date, chunk, context) || + new Date
     var format  = dust.helpers.tap(params.format, chunk, context)
     var lan = dust.helpers.tap(params.lan, chunk, context) || 'en-US'
     date = +date || date
